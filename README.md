2dc6a731b8377c4be47d1b3e0efe6fd9606c9bcf
EGS <sweeny.elizabeth@yahoo.com>
2019-04-01 19:12:12 -0700

    whole entire hw

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# hangman_VOL2
-fingers crossed
+# hangman
+HW 3
---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+<script>
+  
+var x = document.getElementByID("definition");
+document.getElementById("definition").innerHTML = "Paragraph changed!";
+
+</script>
